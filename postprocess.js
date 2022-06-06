e99418c01e375130332ec9d56eb5cefbd0a80b44
Bas Peters <bas.peters@github.com>
2022-06-06 20:04:48 +0200

    Update postprocess.js

--- postprocess.js
+++ postprocess.js
@@ -1,5 +1,5 @@
 import { parseFeed } from "https://deno.land/x/rss/mod.ts";
 
 const filename = Deno.args[0];
-const feed = await parseFeed(filename);
+const feed = parseFeed(filename);
 console.log(feed);
