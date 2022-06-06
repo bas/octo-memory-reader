b23c40b63f0675ac02c65951639410bc12e4274a
Bas Peters <bas.peters@github.com>
2022-06-06 20:13:03 +0200

    Update postprocess.js

--- postprocess.js
+++ postprocess.js
@@ -12,4 +12,10 @@ const xml = await response.text();
 console.log(xml);
 
 // Optional destructuring assignment
-// const { entries } = await parseFeed(xml);
+const { entries } = await parseFeed(xml);
+
+// Access fields using the DublinCore and MediaRss enums
+const dcTitle = entries[0][DublinCore.Title];
+const mediaContent = entries[0][MediaRss.Content];
+
+console.log(mediaContent);
