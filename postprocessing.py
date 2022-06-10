0e657797da87eff4570fb32f0c5fee4cfb8fdf35
Bas Peters <bas.peters@github.com>
2022-06-10 21:01:56 +0200

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -16,7 +16,7 @@ for entry in feed.entries:
   
   if (ext == ".png"):
     image = Image.open(requests.get(url, stream=True).raw)
-    image.thumbnail((200, 200))
+    image.resize((200, 200))
     name_and_path = os.path.join("public/img", name)
     image.save(name_and_path, exist_ok=True)
     images.append(f'{{ src: "/img/{name}", matched: false }},')
