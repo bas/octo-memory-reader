4f1e5791c743ba3b3298304757e7dd0ce6d1d0d2
Bas Peters <bas.peters@github.com>
2022-06-10 20:00:40 +0200

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -3,17 +3,27 @@ from PIL import Image
 
 feed = feedparser.parse(r'./atom.xml')
 
-entries = feed.entries
-for entry in entries:
+os.makedirs("public/img", exist_ok=True)
+os.makedirs("images", exist_ok=True)
+
+images = []
+
+for entry in feed.entries:
   soup = bs4.BeautifulSoup(entry.content[0].value, "html.parser")
-  url = soup.img['src']
-  name = os.path.basename(url)
+  url = soup.img["src"]
+  name = os.path.basename(url).lower()
   ext = os.path.splitext(name)[1]
-  os.makedirs("public/img", exist_ok=True)
   
   if (ext == ".png"):
     image = Image.open(requests.get(url, stream=True).raw)
     image.thumbnail((200, 200))
-    name_and_path = os.path.join("public/img", name.lower())
+    name_and_path = os.path.join("public/img", name)
     image.save(name_and_path, exist_ok=True)
+    images.append(f'{{ src: "/img/{name}", matched: false }},')
 
+with open("images/images.ts", "w+") as f:
+  f.write("const allImages = [\n  ")
+  f.write('\n  '.join(images))
+  f.write("\n]")
+  f.write("\n\nexport default allImages;")
+  f.close()
